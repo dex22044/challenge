fcbbaa7c45f261dfe5d217bdffbbcc42efc7e23c
sashazvereva <59964462+sashazvereva@users.noreply.github.com>
2020-11-25 01:58:13 +0300

    Я сделаль слайдер
    Но 2 часа заморачивалась чтоб упростить код, но по итогу тупо убила время

--- main.js
+++ main.js
@@ -1,37 +1,6 @@
-/*function hidemenu(){
-    let elements_menu = document.querySelectorAll('.nav_link');
-    let menu = document.querySelector('.menu');
-    let content = document.querySelector('.content');
+$(document).ready(main); 
 
-    if(menu.main.width == '30%'){
-        menu.main.width = '5%';
-        content.main.margin = '0 0 0 200px';
-    } else {
-        menu.main.width = '30%';
-        content.main.margin = '0 0 0 50px';
-    }
-
-    for (let elem of elements_menu) {
-        if(elem.main.display == 'none')
-            elem.main.display = 'flex';
-        else
-            elem.main.display = 'none';
-    }
-
-    $('#menu-button').click(function() {
-        $('#nav').toggleClass('visible');
-    });
-}*/
-
-
-function music_off(image) {
-    if (document.getElementById("image").src.indexOf("css/audio_off.png")>0)
-            {document.getElementById("image").src="css/audio.png"}
-        else
-            {document.getElementById("image").src="css/audio_off.png"}
-}
-
-
+/* MENU */
 /* Open menu */
 function openMenu() {
     $('.menu').animate({
@@ -53,6 +22,43 @@ function closeMenu() {
     }, 400);
 };
 
+/* END MENU */
+
+
+
+
+
+/* CONTENT */
+
+/* Change icon music */
+function music_off(image) {
+    if (document.getElementById("image").src.indexOf("css/audio_off.png")>0)
+            {document.getElementById("image").src="css/audio.png"}
+        else
+            {document.getElementById("image").src="css/audio_off.png"}
+}
+
+/* Bxslider */
+jQuery(document).ready(function bxslider(){
+        jQuery('.bxslider').bxSlider({
+           pager: false,
+           captions: true,
+           nextText: '',
+           prevText: '',
+           useCSS: false,
+           easing: 'jswing',
+           speed: 500
+           });
+        });
+
+/* END CONTENT */
+
+
+
+
+
+/* TOPICS */
+
 /* Open relevant topics  */
 function openTopics() {
     $('.topics').animate({
@@ -73,4 +79,5 @@ function closeTopics() {
         left: '0px'
     }, 400);
 };
-$(document).ready(main); 
+
+/* END TOPICS */
