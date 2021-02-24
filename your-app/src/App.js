9c49c310e9a621a4a0d554fa97058f21ebc25549
sashazvereva <59964462+sashazvereva@users.noreply.github.com>
2021-02-25 00:45:35 +0300

    ...

---
+++ your-app/src/App.js
@@ -0,0 +1,26 @@
+
+
+function App() {
+  return (
+    <div className='header'>
+      <div className='container'>
+        <div className='header_inner'>
+          <div className='header_logo'>She loves sweets</div>
+          <nav className='nav'>
+            <a className='nav_link'>Главная</a>
+            <a className='nav_link'>Преимущества</a>
+            <a className='nav_link'>Торты</a>
+            <a className='nav_link'>Пироженые</a>
+            <a className='nav_link'>Капкейки</a>
+            <a className='nav_link'>Начинки</a>
+            <a className='nav_link'>Доставка и оплата</a>
+            <a className='nav_link'>Контакты</a>
+          </nav>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default App;
